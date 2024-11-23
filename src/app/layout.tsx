6c06b9abbe10e111a6e0ae7a32cebacfe17f3bf2
Keiran <keiran0@proton.me>
2024-11-23 01:08:43 +0000

    Updated metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,8 +7,35 @@ import { GridBackground } from "@/components/GridBackground";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Keiran.Live - Portfolio",
-  description: "Software Engineer",
+  title: "My bio :3",
+  description: "Got bored and made this lol",
+  icons: {
+    icon: "/profile.gif",
+  },
+  openGraph: {
+    images: "/profile.gif",
+    title: "My bio :3",
+    description: "Got bored and made this lol",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "My bio :3",
+    description: "Got bored and made this lol",
+    images: "/profile.gif",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      noimageindex: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
 };
 
 export default function RootLayout({
